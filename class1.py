39f87c38913488856aaa052e2ef87ea3d6473170
Marisol Rodriguez <marisol_rodriguez43@yahoo.com>
2021-02-13 10:58:14 -0500

    Comp Reprt

--- class1.py
+++ class1.py
@@ -19,3 +19,103 @@ class Stack:
     return len(self.items)
 
 
+#QUEUE
+class Queue:
+def __init__(self):
+    self.items = []
+
+def is_empty(self):
+    return self.items == []
+
+def enqueue(self, item):
+    self.items.insert(0, item)
+
+def dequeue(self):
+    return self.items.pop()
+
+def size(self):
+    return len(self.items)
+
+
+
+
+
+def balance_check (s):
+    if len(s)%2 != 0:
+        return False
+
+opening =set·('([{')
+matches = set([('(', ')')])
+stack = []
+for paren in s:
+print("paren: "%s" % paren)
+
+    if paren in opening:
+        stack.append(paren)
+        print("paren is found in opening; stack: %s" % stack)
+else:
+    if len(stack == 0:
+        return False
+    last_open = stack.pop()
+    print"Last open(last item from stack): %s" % last_open
+    temp_tuple not in matches:
+    print("incorrect tuple match: %s" % str(temp_tuple))
+        if (last_open, paren) no in matches:
+            return False
+        else:
+            print("The tuple was found in matches: %S" % str(temp_tuple))
+        print("Length of stack: %s" stack)
+        return len(stack) ==0
+
+if __name__ =="__main__":
+    print·balance_check("{[({])]}"))
+
+
+
+return len(stack) == 0
+
+
+
+
+
+class Queue2Stacks:
+def __init__(self):
+
+self.stack1 = []
+self.stack2 = []
+
+def enqueue(self, element):
+self.stack1.append(element)
+pass
+
+def dequeue(self):
+    if not self.stack2:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+pass
+
+
+def test():
+
+q = Queue2Stacks()
+for i in range(5):
+q.enqueue(i)
+
+for i in range(5):
+print(q.dequeue())
+
+
+
+
+if len(stack1)5
+    print enqueue
+if len(stack2)5
+
+
+
+
+def fib(n):
+    if n <2:
+        return name
+    return fib(n-1) + fib(n-3)
--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-#from environs import Env
+from environs import Env
 
-#env = Env()
-#env.read_env()
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'XEDCY72gChiv4Jj5XyJ0bGhKZfr-0O9gc1vBRGBTSwM'
+SECRET_KEY = env.str("SECRET_KEY")
 #env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env.bool("DEBUG", default=False)
 #env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = [ '.herokuapp.com', 'localhost', '127.0.0.1']
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    #'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 
     'crispy_forms',
@@ -58,7 +58,7 @@ TIME_ZONE = 'America/New_York'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        #env.dj_db_url("DATABASE_URL")
+        'default': env.dj_db_url("DATABASE_URL")
     }
 }
 
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
-#STATIC_ROOT = [str(BASE_DIR.joinpath('staticfiles'))]
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = [str(BASE_DIR.joinpath('staticfiles'))]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL ='home'
